47112e11d2aafe7bc75206895b186ba63895cfdd
Andrew Wu <awu@advancelocal.com>
2022-04-10 13:10:14 -0500

    Login + Signup views created. App linked to back4app

--- app/src/main/java/com/example/kinderdorf/LoginActivity.kt
+++ app/src/main/java/com/example/kinderdorf/LoginActivity.kt
@@ -1,12 +1,58 @@
 package com.example.kinderdorf
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseUser
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Check if user logged in
+        //If there is, take them to main
+//        if(ParseUser.getCurrentUser() != null){
+//            goToMainActivity()
+//        }
+        
+        findViewById<Button>(R.id.login_button).setOnClickListener { 
+            val userName = findViewById<EditText>(R.id.etUsername).text.toString()
+            val passWord = findViewById<EditText>(R.id.etPassword).text.toString()
+            loginUser(userName, passWord)
+        }
+        findViewById<Button>(R.id.registerBtn).setOnClickListener {
+            goToRegister()
+        }
     }
 
+    private fun loginUser(userName: String, passWord: String) {
+        ParseUser.logInInBackground(userName, passWord, ({ user, e->
+            if(user != null){
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error Logging In", Toast.LENGTH_SHORT).show()
+            }
+        }))
+    }
+
+    private fun goToRegister(){
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun goToMainActivity(){
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    companion object{
+        const val TAG = "LoginActivity"
+    }
 }
--- app/src/main/java/com/example/kinderdorf/MainActivity.kt
+++ app/src/main/java/com/example/kinderdorf/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kinderdorf
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,30 +10,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kinderdorf.databinding.ActivityMainBinding
+import com.parse.ParseObject
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
     }
-
-
 }
