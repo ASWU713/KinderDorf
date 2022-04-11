f72219569fd1ff4b55d4303769b259799970a8f3
blith <garciwi@oregonstate.edu>
2022-04-11 17:03:10 -0500

    Finished the layout for the Personal Calendar Fragment. Attempting to link the buttonToggleGroup to Toast but it's not currently working. Will fix by next commit.

--- app/src/main/java/com/example/kinderdorf/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/kinderdorf/fragments/CalendarFragment.kt
@@ -5,19 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.kinderdorf.R
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 
 class CalendarFragment : Fragment() {
 
-
+// tells the fragment which layout to use
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
+
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // set onClickListeners and handle logic
+
+        MaterialButtonToggleGroup.OnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked){
+                when (checkedId) {
+                    R.id.allEventsButton -> showToast("All Events Clicked")
+                    R.id.bookedEventsButton -> showToast("Booked Events clicked")
+                    R.id.openRequestButton -> showToast("Open Requests clicked")
+                }
+
+            }
+        }
+    }
+
+    private fun showToast(str:String) {
+        Toast.makeText(requireContext(), str, Toast.LENGTH_SHORT).show()
+    }
 
 }
