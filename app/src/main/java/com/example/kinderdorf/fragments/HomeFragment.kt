191272f767d769f466f2ad6b9f313e2b7407e824
nervster <npsheth93@live.com>
2022-04-17 17:39:30 -0500

    Added Group Calendar Layout, adapter, item layout

--- app/src/main/java/com/example/kinderdorf/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/kinderdorf/fragments/HomeFragment.kt
@@ -7,11 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kinderdorf.R
 import com.example.kinderdorf.Transactions
 import com.example.kinderdorf.adapters.GroupCalendarAdapter
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.parse.FindCallback
 import com.parse.ParseException
 import com.parse.ParseQuery
@@ -24,6 +26,7 @@ class HomeFragment : Fragment() {
     lateinit var groupCalRV : RecyclerView
     lateinit var adapter: GroupCalendarAdapter
     var allTransactions: ArrayList<Transactions> = ArrayList()
+    lateinit var toggleGroup: MaterialButtonToggleGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,18 +43,36 @@ class HomeFragment : Fragment() {
         adapter = GroupCalendarAdapter(requireContext(), allTransactions)
         groupCalRV.adapter = adapter
         groupCalRV.layoutManager = LinearLayoutManager(requireContext())
+        toggleGroup = view.findViewById(R.id.homeToggleButtonGroup)
+        toggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                when(checkedId) {
+                    R.id.scheduledButton -> {
+                        Toast.makeText(requireContext(), "Scheduled Button Clicked", Toast.LENGTH_SHORT).show()
+                        queryScheduledTransactions()
+                    }
+                    R.id.openButton -> {
+                        Toast.makeText(requireContext(), "Open Button Clicked", Toast.LENGTH_SHORT).show()
+                        queryOpenTransactions()
+                    }
+                }
+            }
+        }
 
-        queryTransactions()
+
+        queryScheduledTransactions()
 
     }
 
-    open fun queryTransactions() {
+    open fun queryScheduledTransactions() {
         val query: ParseQuery<Transactions> = ParseQuery.getQuery(Transactions::class.java)
         query.limit = 50
+        query.include("sellerUser")
+        query.include("buyerUser")
         query.orderByAscending("dateRequest")
-//        query.whereExists("userSeller")
-//        query.whereExists("userBuyer")
-        query.whereGreaterThanOrEqualTo("dateRequest", Date())
+        query.whereExists("sellerUser")
+        query.whereExists("buyerUser")
+//        query.whereGreaterThanOrEqualTo("dateRequest", Date())
         query.findInBackground(object : FindCallback<Transactions> {
             override fun done(transactions: MutableList<Transactions>?, e: ParseException?) {
 
@@ -60,17 +81,18 @@ class HomeFragment : Fragment() {
                     e.printStackTrace()
                 } else {
                     Log.i(TAG, "No errors fetching transactions")
-
+                    Log.i(TAG, "Transaction Size: " + transactions?.size.toString())
                     if (transactions != null) {
                         for (transaction in transactions) {
                             var list: List<String> = transaction.getDateRequest().toString().split(" ")
-                            Log.i(TAG, "Seller: " + transaction.getUserSeller()?.username +
+                            Log.i(TAG, "Seller: " + transaction.getUserSeller()?.get("firstName") +
                                             "Date Requested: " + list[1] + "-" + list[2] +
                                             "Price: " + transaction.getPrice() +
                                             "Buyer: " + transaction.getUserBuyer()?.username
                             )
 
                         }
+                        allTransactions.clear()
                         allTransactions.addAll(transactions)
                         adapter.notifyDataSetChanged()
 
@@ -79,6 +101,49 @@ class HomeFragment : Fragment() {
             }
         })
     }
+
+    open fun queryOpenTransactions() {
+        val query: ParseQuery<Transactions> = ParseQuery.getQuery(Transactions::class.java)
+        query.limit = 50
+        query.include("sellerUser")
+        query.include("buyerUser")
+        query.orderByAscending("dateRequest")
+        query.whereExists("sellerUser")
+        query.whereDoesNotExist("buyerUser")
+//        query.whereGreaterThanOrEqualTo("dateRequest", Date())
+        query.findInBackground(object : FindCallback<Transactions> {
+            override fun done(transactions: MutableList<Transactions>?, e: ParseException?) {
+
+                if (e != null) {
+                    Log.e(TAG, "Error fetching transactions")
+                    e.printStackTrace()
+                } else {
+                    Log.i(TAG, "No errors fetching transactions")
+                    Log.i(TAG, "Transaction Size: " + transactions?.size.toString())
+                    if (transactions != null) {
+                        for (transaction in transactions) {
+                            var list: List<String> = transaction.getDateRequest().toString().split(" ")
+                            Log.i(TAG, "Seller: " + transaction.getUserSeller()?.username +
+                                    "Date Requested: " + list[1] + "-" + list[2] +
+                                    "Price: " + transaction.getPrice() +
+                                    "Buyer: " + transaction.getUserBuyer()?.username
+                            )
+
+                        }
+                        allTransactions.clear()
+                        allTransactions.addAll(transactions)
+                        adapter.notifyDataSetChanged()
+
+                    }
+                }
+            }
+        })
+    }
+
+//    open fun noEventsTransaction():Transactions {
+//        var transact: Transactions = Transactions()
+//
+//    }
 
     companion object {
         const val TAG = "HOME_FRAGMENT"
