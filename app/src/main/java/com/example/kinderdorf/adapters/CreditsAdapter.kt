a3f4075bd8925aed0d2da66711315e07ea4a82ef
Andrew Wu <awu@advancelocal.com>
2022-04-18 18:45:48 -0500

    Profile adapter updates

---
+++ app/src/main/java/com/example/kinderdorf/adapters/CreditsAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.kinderdorf.adapters
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kinderdorf.R
+import com.example.kinderdorf.Transactions
+import com.example.kinderdorf.User
+import com.example.kinderdorf.fragments.ProfileFragment.Companion.TAG
+import java.text.SimpleDateFormat
+
+class CreditsAdapter(val context: Context, val transactions: List<Transactions>)
+    : RecyclerView.Adapter<CreditsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_transactions, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val transaction = transactions.get(position)
+        holder.bind(transaction)
+    }
+
+    override fun getItemCount(): Int {
+        return transactions.size
+    }
+
+    class ViewHolder(itemView: View) :  RecyclerView.ViewHolder(itemView) {
+        val tvRequestDate: TextView
+        val tvCredits: TextView
+        val simpleDateFormat = SimpleDateFormat("MMM-dd-yyyy")
+//        val tvFirstName: TextView
+//        val tvLastName: TextView
+//        val tvUserName: TextView
+
+        init {
+            tvRequestDate = itemView.findViewById(R.id.tvRequestDate)
+            tvCredits = itemView.findViewById(R.id.tvCredits)
+
+//            tvFirstName = itemView.findViewById(R.id.tvFirstName)
+//            tvLastName = itemView.findViewById(R.id.tvLastName)
+//            tvUserName = itemView.findViewById(R.id.UserName)
+        }
+
+        fun bind(transactions: Transactions) {
+            Log.i(TAG, "DAte:"+ simpleDateFormat.format(transactions.getDateRequest()))
+            tvRequestDate.text = simpleDateFormat.format(transactions.getDateRequest()).toString()
+            tvCredits.text = transactions.getPrice().toString()
+//            tvFirstName.text = user.getFirstName().toString()
+//            tvLastName.text = user.getLastName().toString()
+//            tvUserName.text = user.getUser().toString()
+        }
+
+
+    }
+
+}
--- app/src/main/java/com/example/kinderdorf/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/kinderdorf/fragments/ProfileFragment.kt
@@ -1,22 +1,35 @@
 package com.example.kinderdorf.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kinderdorf.R
 import com.example.kinderdorf.Transactions
-import com.example.kinderdorf.adapters.ProfileAdapter
+import com.example.kinderdorf.User
+import com.example.kinderdorf.adapters.CreditsAdapter
+import com.parse.FindCallback
+import com.parse.ParseException
 import com.parse.ParseQuery
+import com.parse.ParseUser
+import java.util.*
 
 
 class ProfileFragment : Fragment() {
 
-    lateinit var transactionRecyclerView: RecyclerView
     var allTransactions: MutableList<Transactions> = mutableListOf()
-    lateinit var adapter: ProfileAdapter
+    lateinit var creditAdapter: CreditsAdapter
+    lateinit var creditHistory: RecyclerView
+    lateinit var tvFirstName: TextView
+    lateinit var tvLasttName: TextView
+    lateinit var tvUserName: TextView
+    lateinit var tvCredits: TextView
+    lateinit var tvTimeSpent: TextView
 
 
     override fun onCreateView(
@@ -30,8 +43,21 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        creditHistory = view.findViewById(R.id.tvTransHistory)
+        tvFirstName = view.findViewById(R.id.tvFirstName)
+        tvLasttName = view.findViewById(R.id.tvLastName)
+        tvUserName = view.findViewById(R.id.UserName)
+        tvCredits = view.findViewById(R.id.tvCredits)
+        tvTimeSpent = view.findViewById(R.id.tvTimeSpent)
 
+//        tvFirstName = User.getFirstName()
+//        tvFirstName = User.getLastName()
+//        tvUsertName = User.getUser()
+        queryProfile()
 
+        creditAdapter = CreditsAdapter(requireContext(), allTransactions)
+        creditHistory.adapter = creditAdapter
+        creditHistory.layoutManager = LinearLayoutManager(requireContext())
 
         queryTransactions()
     }
@@ -39,14 +65,53 @@ class ProfileFragment : Fragment() {
     private fun queryTransactions() {
         val query: ParseQuery<Transactions> = ParseQuery.getQuery(Transactions::class.java)
 
+//        query.include(Transactions.KEY_USER_BUYER)
+//        query.include(Transactions.KEY_USER_SELLER)
+        query.addDescendingOrder("dateRequest")
+        query.whereLessThanOrEqualTo("dateRequest", Date())
+        query.setLimit(POST_LIMIT)
+        query.findInBackground(object: FindCallback<Transactions>{
+            override fun done(transactions:MutableList<Transactions>?, e:ParseException?){
+                if(e!= null){
+                    Log.e(TAG, "Error getting transactions")
+                } else{
+                    if( transactions!= null){
+                        for (transaction in transactions){
+                            Log.i(TAG, "Party: " + transaction.getUserSeller()?.username )
+                        }
+                        allTransactions.addAll(transactions)
+                        creditAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        })
+    }
 
+    private fun queryProfile(){
 
+        val query: ParseQuery<User> = ParseQuery.getQuery(User::class.java)
+        query.whereContains("name", "Drew")
+//        query.whereEqualTo(User.KEY_USER, ParseUser.getCurrentUser())
 
+        query.findInBackground(object: FindCallback<User>{
+            override fun done(user: MutableList<User>?, e: ParseException?){
+                if(e!= null){
+                    Log.e(TAG, "Error fetching profile data")
+                }
+                else{
+                    if(user!= null){
+                        Log.i(TAG, "here: "+ user)
+                        for(person in user){
+                            Log.i(TAG, "Profile return data: " + person)
+                        }
+                    }
+                }
+            }
+        })
     }
     companion object{
         const val TAG = "ProfileFragment"
         const val POST_LIMIT = 20
     }
-
 
 }
